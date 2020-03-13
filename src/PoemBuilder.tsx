a69753de6399c2b849aac00e0f4347e8061ceca4
Lucas Schenatto <lucasvschenatto@hotmail.com>
2020-03-13 18:40:15 -0300

    changed to full list of verses and fix style

--- src/PoemBuilder.tsx
+++ src/PoemBuilder.tsx
@@ -1,93 +1,80 @@
 import React from 'react'
-import { View, FlatList, Text, StyleSheet, Image } from 'react-native'
-import { createRequire } from 'module'
+import { View, FlatList, Text, StyleSheet, Image, Dimensions } from 'react-native'
+import verses from './assets/verses'
+
 
 type Verse = {
-    text: string
-    image: string
-    key: string
+	text: string
+	image: string
+	key: number|string
 }
-const verses:Array<Verse> = [
-    {
-        text:'A arte e a ciência',
-        image:'001-pt.jpg',
-        key:'001'
-    },
-    {
-        text:'A ciência da arte',
-        image:'002-pt.jpg',
-        key:'002'
-    },
-    {
-        text:'A arte da ciência',
-        image:'003-pt.jpg',
-        key:'003'
-    },
-    {
-        text:'E teu amor com o meu',
-        image:'004-pt.jpg',
-        key:'004'
-    },
-]
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('screen').width;
+const screenHeight = Dimensions.get('screen').height;
 function Item({text}:{text:string}) {
-    return (
-        <View
-            style={styles.item}>
-            <Text
-                style={styles.itemText}
-                >
-                {text}
-            </Text>
-        </View>
-    )
+	return (
+		<View
+			style={styles.item}>
+			<Text
+				style={styles.itemText}
+				>
+				{text}
+			</Text>
+		</View>
+	)
   }
 class PoemBuilder extends React.Component{
-    renderItem({item}:{item:Verse}){
-        return <Item text={item.text}></Item>
-    }
-    render(){
-        return (
-        <View
-            style={styles.container}
-            >
-            
-            <View style={styles.offset} />
-            <View style={styles.list}>
-                <FlatList
-                    data={verses} 
-                    renderItem={this.renderItem}
-                    keyExtractor={(item)=>item.key}
-                />
-            </View>
-        </View>)
-    }
+	renderItem({item}:{item:Verse}){
+		return <Item text={item.text}></Item>
+	}
+	render(){
+		console.log(verses)
+		return (
+		<View
+			style={styles.container}
+			>
+			
+			<View style={styles.offset} />
+			<View style={styles.listView}>
+				<FlatList
+					style={styles.list}
+					data={verses} 
+					renderItem={this.renderItem}
+					keyExtractor={(item)=>item.key.toString()}
+				/>
+			</View>
+		</View>)
+	}
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#1E2233',
-    },
-    backgroundImage:{
-        ...StyleSheet.absoluteFillObject,
-    },
-    offset:{
-        flex: 1,
-    },
-    list:{
-        flex:2,
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        paddingHorizontal: 10
-    },
-    item: {
-        flex:1,
-        margin:5,
-    },
-    itemText:{
-        fontSize: 20,
-        color: '#fafafc'
-    },
+	container: {
+	  flex: 1,
+	  backgroundColor: '#1E2233',
+	},
+	backgroundImage:{
+		...StyleSheet.absoluteFillObject,
+	},
+	offset:{
+		height: screenHeight/3,
+	},
+	listView:{
+		flexDirection: 'row',
+		alignItems: 'flex-start',
+		paddingHorizontal: 10
+	},
+	list:{
+		height: screenHeight/3*2,
+	},
+	item: {
+		flex:1,
+		margin:5,
+	},
+	itemText:{
+		fontSize: 20,
+		color: '#fafafc'
+	},
   })
 
 export default PoemBuilder
